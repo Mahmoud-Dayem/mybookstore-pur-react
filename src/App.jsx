1b4b55a4991c5086e1b7e8035dad3252fd78810a
mahmoud dayem <mahmoud.dayeem@gmail.com>
2025-07-06 18:28:39 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import BooksPage from './views/BooksPage.jsx';
+import SingleBookPage from './views/SingleBookPage.jsx';
+import AddBookPage from './views/AddBookPage.jsx';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+export default function App() {
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<BooksPage />} />
+        <Route path="/books/:id" element={<SingleBookPage />} />
+        <Route path="/add" element={<AddBookPage />} />
+      </Routes>
+    </Router>
+  )
+}
+ 
